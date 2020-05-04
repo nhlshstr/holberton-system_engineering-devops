0642ed4689ec9f70a921132e3c18874fee80c278
Nehal Shastri <nhlshstr@gmail.com>
2020-05-04 12:05:18 -0700

    added task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
-""" Gathers basic data from API """
+""" Return TODO progress """
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    user_json = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1]))
+
+    name = user_json.json().get("name")
+
+    tasks_json = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(argv[1])).json()
+
+    completed_tasks = []
+
+    for i in range(0, len(tasks_json)):
+        if (tasks_json[i].get("completed")) is True:
+            completed_tasks.append(tasks_json[i].get("title"))
+
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          len(completed_tasks),
+                                                          len(tasks_json)))
+
+    for i in range(0, len(completed_tasks)):
+        print("\t {}".format(completed_tasks[i]))
