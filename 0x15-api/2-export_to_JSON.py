2348dcb1c502f8e7ac584b1fe98c67884d28688a
Nehal Shastri <nhlshstr@gmail.com>
2020-05-04 15:48:31 -0700

    added task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Return TODO progress """
+
+import requests
+from sys import argv
+import json
+
+if __name__ == '__main__':
+    user_json = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/".format(argv[1]))
+
+    tasks_json = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(argv[1])).json()
+
+    user_info_dict = {}
+    user_info_dict["{}".format(argv[1])] = []
+    for x in tasks_json:
+        user_info_dict["{}".format(argv[1])].append({
+            'task': x.get("title"),
+            'completed': x.get('completed'),
+            'username': user_json.json().get("username")
+        })
+
+    with open("{}.json".format(argv[1]), 'w') as file_o:
+        json.dump(user_info_dict, file_o)
